c731d6ea02c14845ced2857bcd3857b80c435bb5
Razvan Spatariu <razvanspatariu@gmail.com>
2016-03-06 16:20:20 +0000

    concatanate into one file for easier use

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const gulpSass = require('gulp-sass');
+const gss = require('gulp-shopify-sass');
 
 gulp.task('sandbox', function() {
   gulp.src('sandbox/style.scss')
@@ -7,11 +8,19 @@ gulp.task('sandbox', function() {
     .pipe(gulp.dest('sandbox'));
 });
 
-gulp.task('default', ['sandbox']);
+
+gulp.task('concatenate', function() {
+  gulp.src('src/*.scss')
+    .pipe(gss())
+    .pipe(gulp.dest(''));
+});
+
+
+gulp.task('default', ['sandbox', 'concatenate']);
 
 gulp.task('watch', function(){
   gulp.watch([
     'sandbox/style.scss',
     'src/**/*'
-  ], ['sandbox']);
+  ], ['sandbox', 'concatenate']);
 });
